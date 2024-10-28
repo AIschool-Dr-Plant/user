df6c1a240e31bd53a4b2a424730df6eda7113f33
KMJ <jimminji@naver.com>
2024-10-28 16:36:33 +0900

    test merge

--- src/main/java/com/dr_plant/project/controller/MainController.java
+++ src/main/java/com/dr_plant/project/controller/MainController.java
@@ -2,22 +2,20 @@ package com.dr_plant.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main")
 public class MainController {
 	
-	@GetMapping("/")
+	@GetMapping("/main")
 	public String test() {
 		System.out.println("/main/index");
-		return "main";
+		return "/main/main";
 	}
 	
 	@GetMapping("/main/tables")
 	public String login() {
 		System.out.println("/main/tables");
-		return "tables";
+		return "/main/tables";
 	}
 	
 }
