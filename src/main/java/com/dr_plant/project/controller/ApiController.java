f0b4c5a762984627128dcc725eace466d6f19cbb
OBD <dhqudeh1@gmail.com>
2024-11-06 12:47:07 +0900

    카카오 테스트를 위해 임시로 수정함

--- src/main/java/com/dr_plant/project/controller/ApiController.java
+++ src/main/java/com/dr_plant/project/controller/ApiController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dr_plant.project.entity.SnsrDataTb;
 import com.dr_plant.project.mapper.SensorDataMapper;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
@@ -20,6 +19,7 @@ public class ApiController {
 
 	@Autowired
 	SensorDataMapper sensorDataMapper;
+	
 
 	@GetMapping("/restTest")
 	public String main() {
@@ -35,20 +35,10 @@ public class ApiController {
 
 	@GetMapping("/kakaoLogin")
 	public void kakaoLogin(@RequestParam String code, @RequestParam String state, HttpServletResponse response) {
-		response.addCookie(setCookie("kakao-token", code)); // 쿠키 추가
-		response.addCookie(setCookie("state", state)); // 쿠키 추가
 
 		// 리다이렉트 설정
 		response.setStatus(HttpServletResponse.SC_FOUND); // 302 리다이렉트
 		response.setHeader("Location", "/kakao/me"); // 리다이렉트할 URL 설정
-	}
-	
-	private Cookie setCookie(String key, String value) {
-		Cookie cookie = new Cookie(key, value);
-		cookie.setMaxAge(1 * 60 * 60); // 1시간 동안 유지
-		cookie.setPath("/kakao"); // 쿠키의 유효 경로
-		cookie.setHttpOnly(false); // 클라이언트 자바스크립트에서 접근 가능하도록 설정 (보안 목적)
-		return cookie;
 	}
 
 }
